44f646c96e42fcbd94c6b8e793a7e2940a2d17a0
caicn <caicn@asiainfo.com>
2018-05-18 16:39:27 +0800

    上传mysql地址

--- config/model-prod.js
+++ config/model-prod.js
@@ -1,1 +1,13 @@
 
+module.exports = {
+  'publicUrl': '/public',
+  'path': '/',
+  'secret': 'admin',
+  'maxAge': 604800000,
+  mysql: {
+      database: 'todolist',
+      user: 'root',
+      password: 'root',
+      host: '127.0.0.1'
+  }
+};
